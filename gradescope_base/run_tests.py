779f7f6cf4ef0b45e79ed823ea4b39cbb03b57e9
Haolin (Hailey) Li <107225510+HaileyHL@users.noreply.github.com>
2025-08-09 14:20:18 -0700

    test

--- gradescope_base/run_tests.py
+++ gradescope_base/run_tests.py
@@ -8,8 +8,5 @@ from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
 if __name__ == '__main__':
     suite = unittest.defaultTestLoader.discover('.')
 
-    # 1. Run tests and show on stdout (for debugging)
-    unittest.TextTestRunner(stream=sys.stdout, verbosity=2).run(suite)
-
     with open('/autograder/results/results.json', 'w') as f:
         JSONTestRunner(visibility='visible', stream=f).run(suite)
