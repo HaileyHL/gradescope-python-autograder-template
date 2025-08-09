a522c9c76ed6e157a8cda44f6f44f6be54efe1bd
Haolin (Hailey) Li <107225510+HaileyHL@users.noreply.github.com>
2025-08-09 13:09:44 -0700

    debug

--- gradescope_base/setup.sh
+++ gradescope_base/setup.sh
@@ -11,8 +11,8 @@ apt-get install -y python3 python3-pip python3-dev
 mkdir -p /root/.ssh
 cp ssh_config /root/.ssh/config
 # Make sure to include your private key here
-cp deploy_key /root/.ssh/deploy_key
-chmod 400 /root/.ssh/deploy_key
+cp key /root/.ssh/key
+chmod 400 /root/.ssh/key
 # To prevent host key verification errors at runtime
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 
