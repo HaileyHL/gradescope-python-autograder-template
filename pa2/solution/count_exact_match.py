a6ba12e854b4f5b5e8fd9a5c3fd1afdf2c7497bd
Haolin (Hailey) Li <107225510+HaileyHL@users.noreply.github.com>
2025-08-09 15:07:02 -0700

    adjust unit tests

--- pa2/solution/count_exact_match.py
+++ pa2/solution/count_exact_match.py
@@ -1,5 +1,5 @@
 def count_exact_match(text, query):
-    count = 0
+    count = 1
     for i in range(min(len(text), len(query))):
         if text[i] == query[i]:
             count += 1
--- pa2/test_validate_email.py
+++ pa2/test_validate_email.py
@@ -8,78 +8,78 @@ except Exception as e:
     raise Exception(f'Couldnt import your function validate_email from files. Please double check your file name and function name.') from None
 
 
-
-def assert_equal_with_message(testcase, test_name, expected, actual):
-    try:
-        testcase.assertEqual(actual, expected)
-    except AssertionError:
-        print(f"--- Failed {test_name} ---")
-        print(f"Expected Output:{expected}")
-        print(f"Actual Output:{actual}")
-        raise
-
 # Tests for validate_email
 class test_validate_email(unittest.TestCase):
+
+    def assert_equal_with_message(self,test_name, expected, actual):
+        try:
+            self.assertEqual(actual, expected)
+        except AssertionError:
+            print(f"--- Failed {test_name} ---")
+            print(f"Expected Output:{expected}")
+            print(f"Actual Output:{actual}")
+            raise
+
     # 32 points in total
     @weight(4)
     def test_validate_email1(self):
         stu = validate_email("abc@gmail.com", "gmail.com")
         expected = True
-        assert_equal_with_message(self, "Test valid email 1", expected, stu)
+        self.assert_equal_with_message("Test valid email 1", expected, stu)
 
     @weight(4)
     def test_validate_email2(self):
         stu = validate_email("abcgmail.com", "gmail.com")
         expected = False
-        assert_equal_with_message(self, "Test invalid email 2", expected, stu)
+        self.assert_equal_with_message("Test invalid email 2", expected, stu)
 
     @weight(3)
     def test_validate_email3(self):
         stu = validate_email("abc@@gmail.com", "gmail.com")
         expected = False
-        assert_equal_with_message(self, "Test invalid email 3", expected, stu)
+        self.assert_equal_with_message("Test invalid email 3", expected, stu)
 
     @weight(3)
     def test_validate_email4(self):
         stu = validate_email("abc@:^).com", ":^).com")
         expected = True
-        assert_equal_with_message(self, "Test weird suffix", expected, stu)
+        self.assert_equal_with_message("Test weird suffix", expected, stu)
 
     @weight(3)
     def test_validate_email5(self):
         stu = validate_email("       abc@gmail.com", "gmail.com")
         expected = True
-        assert_equal_with_message(self, "Test leading spaces 5", expected, stu)
+        self.assert_equal_with_message("Test leading spaces 5", expected, stu)
 
     @weight(3)
     def test_validate_email6(self):
         stu = validate_email("abc@gmail.com       ", "gmail.com")
         expected = True
-        assert_equal_with_message(self, "Test trailing spaces", expected, stu)
+        self.assert_equal_with_message("Test trailing spaces", expected, stu)
 
     @weight(3)
     def test_validate_email7(self):
         stu = validate_email("       abc@gmail.com       ", "gmail.com")
         expected = True
-        assert_equal_with_message(self, "Test both spaces 7", expected, stu)
+        self.assert_equal_with_message("Test both spaces 7", expected, stu)
 
     @weight(3)
     def test_validate_email8(self):
         stu = validate_email("ab c@gmail.com", "gmail.com")
         expected = False
-        assert_equal_with_message(self, "Test spaces in the middle 8", expected, stu)
+        self.assert_equal_with_message("Test spaces in the middle 8", expected, stu)
 
     @weight(3)
     def test_validate_email9(self):
         stu = validate_email("abc@g mail.com", "gmail.com")
         expected = False
-        assert_equal_with_message(self, "Test spaces in the middle 9", expected, stu)
+        self.assert_equal_with_message("Test spaces in the middle 9", expected, stu)
 
     @weight(3)
     def test_validate_email10(self):
         stu = validate_email("abc@gmail.com", "ucsd.edu")
         expected = False
-        assert_equal_with_message(self, "Test wrong suffix 10", expected, stu)
+        self.assert_equal_with_message("Test wrong suffix 10", expected, stu)
 
 if __name__ == '__main__':
     unittest.main()
